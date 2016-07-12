2ca101ee1361a8037b26a29c1b52ec72e4b23419
chrisbialecki <chrisbialecki@yahoo.co.uk>
2016-07-12 21:35:51 +0100

    go

---
+++ WPF_UserControl/WPF_UserControl_1/CommandOpenNewWindow.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF_UserControl_1
+{
+    public class CommandOpenNewWindow : ICommand
+    {
+
+        public bool CanExecute(object parameter)
+        {
+            var _student = StudentModel.GetStudentModel();
+            return _student.LastName != null && _student.FirstName != null && _student.Age != null;
+           
+           
+
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+            
+        }
+
+        public void Execute(object parameter)
+        {
+            var _window2 = new Window2();
+            _window2.Show();
+        }
+    }
+}
