105526404a8854ffdbc8c9278cff7a1a1d3b2b13
Krzysztof Bialecki <chrisbialecki@yahoo.co.uk>
2016-07-12 11:34:21 +0100

    fixed after botched pull / merge. Command class re-created.

--- WPF_UserControl/WPF_UserControl_1/CommandOpenNewWindow.cs
+++ WPF_UserControl/WPF_UserControl_1/CommandOpenNewWindow.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace WPF_UserControl_1
 {
-    class CommandOpenNewWindow
+    public class CommandOpenNewWindow : ICommand
     {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }                    
+        }
+
+
+        public bool CanExecute(object parameter)
+        {
+            var _studentDetails = StudentModel.GetStudentModel();
+            return _studentDetails.Age != null && _studentDetails.FirstName != null && _studentDetails.LastName != null;
+        }
+
+        public void Execute(object parameter)
+        {
+            var _window2 = new Window2();
+            _window2.Show();
+        }
     }
 }
